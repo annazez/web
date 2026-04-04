59d3ad69008a540cb631189b5d459fd955c982f6
Anna Zezulka <anna.zezulka@proton.me>
2026-04-04 16:02:26 +0200

    refactor: remove unused parse-check.js file

--- parse-check.js
+++
@@ -1,5 +0,0 @@
-import { parse } from 'yaml';
-import fs from 'fs';
-const en = fs.readFileSync('./src/content/projects/mental-health-app-en.mdx', 'utf-8');
-console.log('YAML block:', en.split('---')[1]);
-console.log('Parsed:', parse(en.split('---')[1]));
