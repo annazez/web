ff36bef2ef0703a544597658c6e49a7dccbbc472
Woodpecker CI <anna@zezulka.me>
2026-04-30 20:54:53 +0000

    deploy: 2026-04-30T20:54:53Z

---
+++ assets/Layout.astro_astro_type_script_index_0_lang.BIdlXqRU.js
@@ -0,0 +1,1 @@
+import{b as r}from"./bind-once.1YrwAl71.js";const e=window;r("theme-after-swap",()=>{document.addEventListener("astro:after-swap",()=>{e.__applyPreferredTheme&&e.__applyPreferredTheme()})});
---
+++ assets/bind-once.1YrwAl71.js
@@ -0,0 +1,1 @@
+function i(o,c){const n=`__bindOnce_${o.replace(/[^a-zA-Z0-9]/g,"_")}`,e=window;e[n]||(c(),e[n]=!0)}export{i as b};
